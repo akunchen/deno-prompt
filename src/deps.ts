e7fcb3d4371b9e363c9d261948dde2047a9de51d
seveibar <seveibar@gmail.com>
2021-07-24 20:06:33 -0700

    utf8 recording

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
 export { BufReader } from "https://deno.land/std@0.59.0/io/bufio.ts";
 export * as Colors from "https://deno.land/std@0.59.0/fmt/colors.ts";
-export * as utf8 from 'https://deno.land/std@v0.59.0/encoding/utf8.ts';
+export * as utf8 from 'https://deno.land/std@0.59.0/encoding/utf8.ts';
